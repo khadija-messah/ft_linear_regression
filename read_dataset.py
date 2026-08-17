51f32d2a31dc8a1d7bf46872369a17fabcceced3
khadija messah <duva@MacBook-Pro-de-khadija.local>
2026-08-17 12:35:29 +0100

    model structure done

--- read_dataset.py
+++ read_dataset.py
@@ -28,28 +28,23 @@ def read_data_and_preprocess(file_path):
         return 
     mileage = data[:, 0]
     price = data[:, 1]
-    print("Original mileage:", mileage)
-    print("Original price:", price)
-    print("--------------------------------\n\n")
-    mileage, mileage_min, mileage_max = normalize_data(mileage)
-    print("Normalized mileage:", mileage)
-    print("denormalized mileage:", denormalize_data(mileage, mileage_min, mileage_max))
 
-    return mileage, price
-
-def training_data(file_path):
-    print("here\n\n")
-    mileage, price = read_data_and_preprocess(file_path)
-    print("here1",mileage," \n\n",price)
+    mileage1, mileage_min, mileage_max = normalize_data(mileage)
     if mileage is None or price is None:
         print("Training data could not be loaded.")
+        return None, None
+    return mileage1, price
+
+def normalizeAndError(file_path):
+    mileage, price = read_data_and_preprocess(file_path)
+    if mileage is None or price is None:
         return None, None
     print("Starting the training process...")
     theta0 = 0
     theta1 = 0
-    m = len(mileage)
-    estimated_price = estimate_price(mileage, theta0, theta1)
-    print("Estimated price:", estimated_price)
+    theta0, theta1 = train_model(mileage, price, theta0, theta1)
+    return theta0, theta1
+
 
 def estimate_price(mileage, theta0, theta1):
     return theta0 + theta1 * mileage
@@ -59,17 +54,31 @@ def normalize_data(mileage):
      return normalized_mileage,mileage.min(), mileage.max()
 
 
-def denormalize_data(normalized_mileage, mileage_min, mileage_max):
-    mileage = normalized_mileage * (mileage_max - mileage_min) + mileage_min
-    return mileage
+def train_model(mileage, price, theta0, theta1, learning_rate=0.01, num_iterations=1000):
+    m = len(mileage)
+    for i in range(num_iterations):
+        estimated_price = estimate_price(mileage, theta0, theta1)
+        error = estimated_price - price
+        sum_error_price = np.sum(error)
+        average_error = sum_error_price / m
+        tmpθ0 = learning_rate * average_error
 
+        sum_error_mileage = np.sum(error * mileage)
+        average_error_mileage = sum_error_mileage / m
+        tmpθ1 = learning_rate * average_error_mileage
 
+        theta0 -= tmpθ0
+        theta1 -= tmpθ1
+    print(f"Training completed. Final parameters: theta0 = {theta0}, theta1 = {theta1}")
+    return theta0, theta1
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 2 or not sys.argv[1].endswith('.csv'):
         print("please enter 1 dataset file name with .csv extension")
         return
     try:
-        training_data(sys.argv[1])
+        theta0, theta1 = normalizeAndError(sys.argv[1])
+        estimated_price = estimate_price(15000, theta0, theta1)
+        print(f"Estimated price for 15000 km: {estimated_price}")
     except Exception as e:
         print("An error occurred:", e)
 main()
